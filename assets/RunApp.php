21b33e1ae0e770ade65dfe7b7a28835e3ee01985
Kue <jongkid@gmail.com>
2014-03-13 16:05:58 +0700

    Friend Online
    !

---
+++ assets/RunApp.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'Friend Online!';
+$path = '';
+?>	
+<?php include_once 'header.php'; ?>
+<div id="fb-root"></div>
+<div id="wrapper">
+<div id="header">
+<b><label style="font-family:UTUEK;">F</label>riend Online</b></div>
+<div id="friends">
+<div style="text-align:center;color:#777;font-size:28px;margin:120px 0px 30px 0px;opacity:0.8;"><a class="loading loading_a"></a></div>
+<div style="text-align:center;color:#777;font-family:UTUEK,tahoma,Arial,sans-serif;font-size:24px;margin:30px 0px 120px 0px;opacity:0.5;">รอสักครู่!</div>
+</div>
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write(unescape('%3Cscript src="/jquery.min.js"%3E%3C/script%3E'))</script>
+<script src="/App.js"></script>
+<script src="/staticJS.js"></script>
+<?php include_once 'foot.php'; ?>
